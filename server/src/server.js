960d3373e7a9b71d921cbc00ca73979016df57dc
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-05-27 17:43:06 +0530

    ganpati bappa morya

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ const httpServer = createServer(app)   // Create an HTTP server
 
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "https://mandal-khata-opacurps4-chaitanyakhandekars-projects.vercel.app/",
+    origin: process.env.CLIENT_URL || "https://mandal-khata-opacurps4-chaitanyakhandekars-projects.vercel.app",
     // origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
